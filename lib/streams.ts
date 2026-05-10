fe8bf607635bacece839a155ddee1a2aa4360b3c
Gg777ok <agusutir22@gmail.com>
2026-05-10 14:01:13 +0700

    update link server

--- lib/streams.ts
+++ lib/streams.ts
@@ -46,9 +46,9 @@ export const DataStreams: Streams[] = [
     type: "dash",
     links: [
       {
-        id: "065051b99bf5cf8d9a3bde5cbde6aaf9",
-        key: "214bd176832872339ce184338320f9a2",
-        url: "https://a201aivottlinear-a.akamaihd.net/OTTB/syd-nitro/live/clients/dash/enc/inpyms8ezu/out/v1/1084d5c9a97a4c5b9f9554c88f486646/cenc.mpd",
+        id: "1539f043249e413d91906036f305831e",
+        key: "671e24fd8d234c7f38d85055815f902a",
+        url: "https://cool-bonus-53bf.streamingkutiga.workers.dev/https://fta2-cdn-flr.visionplus.id/out/v1/46d9cf39b9a84183b8d5022ac8f4bc41/index.mpd",
       },
     ],
     excerpt:
